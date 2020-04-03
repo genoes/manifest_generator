e467cedb9d55b8ca9311851176544c2832e003b6
Geno Sanchez <geno21@ucla.edu>
2020-04-02 21:19:36 -0700

    edited script

--- manifest_generator.py
+++ manifest_generator.py
@@ -9,12 +9,12 @@ id_count = -1
 # creates csv and adds desired headers
 with open('manifest.csv', 'w', newline='') as csvfile:
     fieldnames = ['id','filename']
-    thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    thewriter.writeheader()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
 
 # writes file names and numbers to columns in csv.
     for filename in filenames:
         id_count += 1
-        thewriter.writerow({'id':id_count, 'filename': filename})
+        writer.writerow({'id':id_count, 'filename': filename})
 
 print('\n'"Complete!")
