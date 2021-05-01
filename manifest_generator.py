1a517c5e348c40f6cb9865115a6c2f795781d7ab
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2021-05-01 12:57:32 -0700

    added a line to remove .DS_Store from directory, added utf-8 encoding

--- manifest_generator.py
+++ manifest_generator.py
@@ -3,13 +3,14 @@ import csv, os
 # stores file names in a list
 path = input('\n'"Enter absolute path to directory: ").strip()
 filenames = os.listdir(path)
+filenames.remove(".DS_Store")
 filenames.sort()
 id_count = 0
 
 # creates csv and adds desired headers
-with open('manifest.csv', 'w', newline = '') as csvfile:
+with open('manifest.csv', 'w', newline = '', encoding='utf-8') as csvfile:
     fieldnames = ['id','filename']
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer = csv.DictWriter(csvfile, delimiter=',', fieldnames = fieldnames)
     writer.writeheader()
 
 # writes file names and numbers to columns in csv.
